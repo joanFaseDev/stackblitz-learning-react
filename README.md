aab93a025c2231c6a5c138c706cc2b1d4485022e
joanFaseDev <108225702+joanFaseDev@users.noreply.github.com>
2023-07-08 22:24:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-learning-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-eyznc7)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <Presentation />
+    </div>
+  );
+}
+
+function Presentation() {
+  return (
+    <p>
+      Hi! I'm Jordan, an aspiring web-developer. And I'm currently wondering if
+      I should try back-end technologies as I have become curious about it.
+    </p>
+  );
+}
